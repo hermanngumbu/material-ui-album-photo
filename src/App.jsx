fd9106aa97b78ee8026505ba06cca06288b283e4
hermanngumbu <https://github.com/hermanngumbu.com>
2022-10-01 18:12:23 +0100

    Build body page and display images

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import { Typography, AppBar, Button, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material";
-import { PhotoCamera } from '@mui/icons-material';
-import { makeStyles } from '@material-ui/styles';
-import { createTheme, themeProvider } from '@mui/material/styles';
-import { color } from '@mui/system';
-// import { makeStyles } from '@material-ui/core/styles';
+import { Typography, AppBar, Button, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, CardActions } from "@material-ui/core";
+import { PhotoCamera } from '@material-ui/icons';
+// import { createTheme, themeProvider } from '@mui/material/styles';
 
-const useStyles = makeStyles({
-    container: {
-        backgroundColor: 'backgroundColor.paper',
-        padding: 'spacing(28, 0, 6)',
-        color: 'red'
-    }
-})
+import useStyles from './styles';
 
+const cards = [1,2,3,4,5,5,6,7,8,9];
 const App = () => {
 
     const classes = useStyles();
-    const theme = createTheme();
+    // const theme = createTheme();
 
     return (
-      <themeProvider theme={theme}>
+      <>
         <CssBaseline />
         <AppBar position='relative'>
             <Toolbar>
-                <PhotoCamera />
+                <PhotoCamera className={classes.icon}/>
                 <Typography variant='h6'>
                     Photo Album
                 </Typography>
@@ -36,25 +28,60 @@ const App = () => {
                     <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
                         Photo Album
                     </Typography>
-                    <Typography variant='h5' align='center' color= {classes.container} paragraph>
+                    <Typography variant='h5' align='center' color='textSecondary' paragraph>
                     Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.
                     </Typography>
-                    <div>
+                    <div className={classes.buttons}>
                         <Grid container spacing={2} justifyContent='center'>
                             <Grid item>
-                                <Button variant='contained' color="primary">
+                                <Button variant="contained" color="primary">
                                     See my photo
-                                </Button>
-                                <Button variant='outlined' color="primary">
+                                </Button> 
+                                <Button variant="outlined" color="primary">
                                     Secondary action
                                 </Button>
+                                
                             </Grid>
                         </Grid>
                     </div>
                 </Container>
             </div>
+            <Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                  {cards.map((card) => (
+                     <Grid item key={card} xs={12} sm={6} md={4} lg={4}>
+                     <Card className={classes.card}>  
+                       <CardMedia 
+                         className={classes.cardMedia}
+                         image="https://source.unsplash.com/random"
+                         title="Image title"
+                       />
+                       <CardContent className={classes.cardContent}>
+                         <Typography gutterBottom variant='h5'>
+                         Heading
+                         </Typography>
+                         <Typography>
+                         
+                             This is a media card. You can use this section to describe the content.
+                         </Typography>
+                         <CardActions>
+                             <Button size='small' color='primary'>View</Button>
+                             <Button size='small' color='primary'>Edit</Button>
+
+                         </CardActions>
+                       </CardContent>
+                     </Card>
+                 </Grid>
+                  ))}
+                  </Grid>   
+            </Container>
         </main>
-      </themeProvider>
+        <footer className='footer'>
+            <Typography variant='h6' align='center' gutterBottom>
+                    
+            </Typography>
+        </footer>
+      </>
     )
         
 }
