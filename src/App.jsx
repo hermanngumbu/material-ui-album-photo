0dad2c01ccace9a0e26b35fe6302cba1c4a6cd00
hermanngumbu <https://github.com/hermanngumbu.com>
2022-10-01 18:44:00 +0100

    Added and design a footer page

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,10 @@ const App = () => {
         </main>
         <footer className='footer'>
             <Typography variant='h6' align='center' gutterBottom>
-                    
+                Footer 
+            </Typography>
+            <Typography variant='subtitle1' align='center' color='textSecondary'>
+                Somethings here to give the footer a purpose!
             </Typography>
         </footer>
       </>
